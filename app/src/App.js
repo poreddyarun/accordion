ada1e80b1ab53406b932f4c88194367ad667ad56
poreddyarun <arunkumarporeddy1@gmail.com>
2024-03-27 18:45:18 +0530

    first commit

---
+++ app/src/App.js
@@ -0,0 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import React from 'react';
+import AccordionComponent from './AccordionComponent';
+import './App.css';
+ 
+ 
+function App() {
+  return (
+    <div className="App">
+      <AccordionComponent/>
+ 
+ 
+   
+     
+    </div>
+  );
+}
+ 
+ 
+export default App;
